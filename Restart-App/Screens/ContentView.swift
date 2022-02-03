f6f682ef1daaab6709b3e9d648e421f93d040bf4
Deepanshu Jain <dpnshu95@gmail.com>
2022-02-03 23:15:53 +0530

    Add onboarding and home views

---
+++ Restart-App/Screens/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Restart-App
+//
+//  Created by Deepanshu Jain on 03/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
